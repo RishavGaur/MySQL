9757811e276a3848351f2ba46a6cb060461c3e39
RishavGaur <rishav021gaur@gmail.com>
2024-03-07 17:22:25 +0530

    Performing more operations in table

--- new.sql
+++ new.sql
@@ -15,3 +15,16 @@ INSERT INTO info (roll_no, name, marks, grade, city) VALUES
 (105, "emanuel", 12, "F", "Delhi"),
 (106, "farah", 82, "B", "Delhi");
 SELECT city, AVG(marks) FROM info GROUP BY city ORDER BY AVG(marks) DESC;
+
+/*Now Change the name of column "name" to "full_name"*/
+ALTER TABLE info CHANGE name full_name VARCHAR(50);
+SELECT * FROM info;
+
+/* Now Delete all the students who scored marks less than 80 */
+SET SQL_SAFE_UPDATES= 0;
+DELETE FROM info WHERE marks<80;
+SELECT * FROM info;
+
+/* Now Deleting column for grade */
+ALTER TABLE info DROP COLUMN grade;
+SELECT * FROM info;
